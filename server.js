d2cde231e31e0a54d5dabb12fd203397e2bda5d6
nitikshh <122013711+nitikshh@users.noreply.github.com>
2023-09-20 18:51:01 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,103 +1,71 @@
-const express = require('express');
-const app = express();
-const http = require('http').createServer(app);
-const multer = require('multer');
-const path = require('path');
 const fs = require('fs');
 const csv = require('csv-parser');
+const express = require('express');
+const multer = require('multer');
+const path = require('path');
+const app = express();
+const port = 3000;
 
-const PORT = process.env.PORT || 3000;
-const CSV_FILE_PATH = path.join(__dirname, 'uploads', 'video_links.csv'); // Path to the CSV file
-
-// Create the "uploads" folder if it doesn't exist
-const UPLOADS_FOLDER = path.join(__dirname, 'uploads');
-if (!fs.existsSync(UPLOADS_FOLDER)) {
-    fs.mkdirSync(UPLOADS_FOLDER);
-}
-
-// Set up Multer for handling file uploads
+// Set up multer for file uploads
 const storage = multer.diskStorage({
-    destination: (req, file, cb) => {
-        cb(null, 'uploads/'); // Save uploaded videos and images to the "uploads" folder
-    },
-    filename: (req, file, cb) => {
-        const ext = path.extname(file.originalname);
-        cb(null, Date.now() + ext); // Rename the file with a unique timestamp
-    },
+  destination: 'uploads',
+  filename: (req, file, cb) => {
+    cb(null, Date.now() + path.extname(file.originalname));
+  },
 });
 
-const upload = multer({ storage });
+const upload = multer({ storage: storage });
 
-// Initialize the videos array by reading data from the CSV file
-const videos = [];
+// Serve static files from the 'public' directory
+app.use(express.static('public'));
 
-fs.createReadStream(CSV_FILE_PATH)
-    .pipe(csv())
-    .on('data', (row) => {
-        videos.push(row);
-    })
-    .on('end', () => {
-        console.log('CSV file loaded:', videos);
+// Use JSON middleware for parsing request bodies
+app.use(express.json());
+
+// Define the route for uploading videos and posters
+app.post('/upload', upload.fields([{ name: 'video', maxCount: 1 }, { name: 'poster', maxCount: 1 }]), (req, res) => {
+  const videoFile = req.files['video'][0];
+  const posterFile = req.files['poster'][0];
+  const title = req.body.title;
+
+  // Generate the paths for video and poster files
+  const videoPath = '/uploads/' + videoFile.filename;
+  const posterPath = '/uploads/' + posterFile.filename;
+
+  // Create or append to the CSV file with column names if it doesn't exist
+  const csvData = [
+    {
+      Title: 'Title',
+      VideoLink: 'VideoLink',
+      PosterLink: 'PosterLink',
+    },
+    {
+      Title: title,
+      VideoLink: videoPath,
+      PosterLink: posterPath,
+    },
+  ];
+
+  const csvFilePath = 'videos.csv';
+
+  fs.stat(csvFilePath, (err, stat) => {
+    const fileExists = !err;
+
+    csvData.forEach((data, index) => {
+      if (!fileExists && index === 0) {
+        // If the file doesn't exist, write the header row
+        fs.writeFileSync(csvFilePath, `${Object.keys(data).join(',')}\n`);
+      }
+
+      // Append data to the CSV file
+      fs.appendFileSync(csvFilePath, `${Object.values(data).join(',')}\n`);
     });
 
-// Serve static files from the "public" folder
-app.use(express.static(__dirname + '/public'));
-
-// Serve uploaded videos and images
-app.use('/uploads', express.static(__dirname + '/uploads'));
-
-app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/index.html');
+    res.send('File uploaded successfully.');
+  });
 });
 
-app.get('/upload', (req, res) => {
-    res.sendFile(__dirname + '/upload.html');
-});
-
-// Handle video uploads
-app.post('/upload', upload.fields([{ name: 'video', maxCount: 1 }, { name: 'poster', maxCount: 1 }]), (req, res) => {
-    const title = req.body.title;
-    const videoFile = req.files['video'][0];
-    const posterFile = req.files['poster'][0];
-
-    // Save the video, poster, and title to the uploads folder
-    const videoPath = path.join(__dirname, 'uploads', videoFile.filename);
-    const posterPath = path.join(__dirname, 'uploads', posterFile.filename);
-
-    fs.renameSync(videoFile.path, videoPath);
-    fs.renameSync(posterFile.path, posterPath);
-
-    // Add video data to the videos array and save to the CSV file
-    const videoData = {
-        Title: title,
-        VideoLink: `/uploads/${videoFile.filename}`,
-        PosterLink: `/uploads/${posterFile.filename}`,
-    };
-
-    videos.push(videoData);
-
-    fs.appendFileSync(CSV_FILE_PATH, `${videoData.Title},${videoData.VideoLink},${videoData.PosterLink}\n`);
-
-    // Redirect back to the main page after uploading
-    res.redirect('/');
-});
-
-app.get('/video-list', (req, res) => {
-    // Send the videoList as JSON response
-    res.json(videos);
-});
-
-
-// Generate and save the CSV file
-app.get('/generate-csv', (req, res) => {
-    const csvData = videos.map(video => `${video.Title},${video.VideoLink},${video.PosterLink}`).join('\n');
-
-    // Save the CSV to a file
-    fs.writeFileSync(CSV_FILE_PATH, csvData);
-
-    res.send('CSV file generated and saved.');
-});
-
-http.listen(PORT, () => {
-    console.log(`Listening on port ${PORT}`);
+// Start the server
+app.listen(port, () => {
+  console.log(`Listening on port ${port}`);
 });
