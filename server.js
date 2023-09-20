4e007391d335e6e2514c35554fa2d894441d5af8
nitikshh <122013711+nitikshh@users.noreply.github.com>
2023-09-20 18:59:13 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,71 +1,46 @@
+const express = require('express');
 const fs = require('fs');
 const csv = require('csv-parser');
-const express = require('express');
 const multer = require('multer');
-const path = require('path');
+const upload = multer({ dest: 'uploads/' });
+
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
-// Set up multer for file uploads
-const storage = multer.diskStorage({
-  destination: 'uploads',
-  filename: (req, file, cb) => {
-    cb(null, Date.now() + path.extname(file.originalname));
-  },
-});
-
-const upload = multer({ storage: storage });
-
-// Serve static files from the 'public' directory
+app.use(express.json());
 app.use(express.static('public'));
 
-// Use JSON middleware for parsing request bodies
-app.use(express.json());
+app.get('/video-list', (req, res) => {
+  const results = [];
+  fs.createReadStream('videos.csv')
+    .pipe(csv())
+    .on('data', (data) => {
+      results.push(data);
+    })
+    .on('end', () => {
+      console.log('CSV file loaded:', results);
+      res.json(results);
+    });
+});
 
-// Define the route for uploading videos and posters
 app.post('/upload', upload.fields([{ name: 'video', maxCount: 1 }, { name: 'poster', maxCount: 1 }]), (req, res) => {
   const videoFile = req.files['video'][0];
   const posterFile = req.files['poster'][0];
   const title = req.body.title;
 
-  // Generate the paths for video and poster files
-  const videoPath = '/uploads/' + videoFile.filename;
-  const posterPath = '/uploads/' + posterFile.filename;
+  // Create column names if the CSV file doesn't exist
+  if (!fs.existsSync('videos.csv')) {
+    fs.writeFileSync('videos.csv', 'Title,VideoLink,PosterLink\n');
+  }
 
-  // Create or append to the CSV file with column names if it doesn't exist
-  const csvData = [
-    {
-      Title: 'Title',
-      VideoLink: 'VideoLink',
-      PosterLink: 'PosterLink',
-    },
-    {
-      Title: title,
-      VideoLink: videoPath,
-      PosterLink: posterPath,
-    },
-  ];
+  // Append data to the CSV file
+  const videoLink = `/uploads/${videoFile.filename}`;
+  const posterLink = `/uploads/${posterFile.filename}`;
+  fs.appendFileSync('videos.csv', `${title},${videoLink},${posterLink}\n`);
 
-  const csvFilePath = 'videos.csv';
-
-  fs.stat(csvFilePath, (err, stat) => {
-    const fileExists = !err;
-
-    csvData.forEach((data, index) => {
-      if (!fileExists && index === 0) {
-        // If the file doesn't exist, write the header row
-        fs.writeFileSync(csvFilePath, `${Object.keys(data).join(',')}\n`);
-      }
-
-      // Append data to the CSV file
-      fs.appendFileSync(csvFilePath, `${Object.values(data).join(',')}\n`);
-    });
-
-    res.send('File uploaded successfully.');
-  });
+  res.status(200).send('Data saved successfully.');
 });
 
-// Start the server
 app.listen(port, () => {
   console.log(`Listening on port ${port}`);
 });
